f38ff489d29f136df74600bb695da0e77ec158f6
Lesly Kinzel <lesly.helios@proton.me>
2025-05-26 09:40:04 +1000

    Initial page layout

--- src/server.py
+++ src/server.py
@@ -6,6 +6,16 @@ app = Flask(__name__)
 def route_home():
     return render_template("home/index.html")
 
-@app.route("/drafts")
-def route_drafts():
-    return render_template("drafts/index.html")
+@app.route("/heroes")
+def route_heroes():
+    return render_template("heroes/index.html")
+
+@app.route("/heroes/meta")
+def route_heroes_meta():
+    return render_template("heroes/meta/index.html")
+
+@app.route("/heroes/draft")
+def route_heroes_draft():
+    return render_template("heroes/draft/index.html")
+
+
