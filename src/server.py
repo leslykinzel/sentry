a478031ee76b6d13718ec560d4d304dc9baf016f
Lesly Kinzel <lesly.helios@proton.me>
2025-05-26 00:09:31 +1000

    Rename _macros > _globals

--- src/server.py
+++ src/server.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def r_home():
+def route_home():
     return render_template("home/index.html")
 
 @app.route("/drafts")
-def r_draft():
+def route_drafts():
     return render_template("drafts/index.html")
