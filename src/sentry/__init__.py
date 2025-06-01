f4fe5297bbb58b71ff7f1a3c4e5399f95bb734b1
Lesly Kinzel <lesly.helios@proton.me>
2025-06-01 15:35:40 +1000

    Rate limiting etc.

--- src/sentry/__init__.py
+++ src/sentry/__init__.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from flask_caching import Cache
+import opendota
 
 
 def create_app():
 
     """ Application config """
 
+    config = {
+        "DEBUG": True,
+        "CACHE_TYPE": "SimpleCache",
+        "CACHE_DEFAULT_TIMEOUT": 60
+    }
+
     app = Flask(__name__)
+    app.config.from_mapping(config)
+
+    cache = Cache(app)
+    limiter = Limiter(get_remote_address, app=app)
+
+    api = opendota.OpenDota()
 
     """ Route definitions """
 
@@ -28,6 +44,12 @@ def create_app():
     def route_heroes_draft():
         return render_template("heroes/draft/index.html")
 
+
+    @cache.cached(timeout=30)
+    @limiter.limit("10 per minute")
+    @app.route("/api/live-games")
+    def api_live_games():
+        return jsonify(api.get_live())
 
     return app
 
--- src/sentry/core/opendota.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-def get_heroes():
-    pass
