4eb427b62cc8c5a07eba65d16ae274590cb4d017
Lesly Kinzel <lesly.helios@proton.me>
2025-06-18 19:33:21 +1000

    Implement live games display, add missing hero portraits

--- src/sentry/core/ui/live_games.py
+++ src/sentry/core/ui/live_games.py
@@ -21,14 +21,22 @@ class LiveGames:
 
         for game in response:
             try:
-                if game.get("is_watch_eligible", False) == True:
+                players = game.get("players")
+                if ( 
+                    # if a game is not full, ignore it
+                    game.get("is_watch_eligible", False)
+                    and players
+                    and len(players) == 10
+                ):
                     live_games.append(
                         LiveDotaGame(
+                            match_id=game.get("match_id", 0),
                             avg_mmr=game.get("average_mmr", 0),
                             r_score=game.get("radiant_score", 0),
                             d_score=game.get("dire_score", 0),
                             game_time=game.get("game_time", 0),
-                            players=game.get("players", None)
+                            gold_lead=game.get("radiant_lead", 0),
+                            players=players
                         )
                     )
             except Exception:
@@ -38,20 +46,22 @@ class LiveGames:
 
     def as_html(self):
         if self.games is None:
-            return "<p>No live games available</p>"
+            return "<p>No games available, try again later.</p>"
 
-        top_games = sorted(self.games, reverse=True)[:9]
+        top_games = sorted(self.games, reverse=True)[:6]
 
         return "".join(str(game) for game in top_games)
 
 
 class LiveDotaGame:
 
-    def __init__(self, avg_mmr, r_score, d_score, game_time, players):
+    def __init__(self, match_id, avg_mmr, r_score, d_score, game_time, gold_lead, players):
+        self.match_id = match_id
         self.avg_mmr = avg_mmr
         self.r_score = r_score
         self.d_score = d_score
         self.game_time = game_time
+        self.gold_lead = gold_lead
         self.players = self.get_players_summary(players)
 
     def __lt__(self, other: "LiveDotaGame"):
@@ -62,20 +72,29 @@ class LiveDotaGame:
         secs = self.game_time % 60
         time = f"{mins}:{secs:02d}"
 
+        radiant_players = self.players["radiant"]
+        dire_players = self.players["dire"]
+
         player_rows = ""
-        for i in range(5):
-            radiant = self.players[i]
-            dire = self.players[i + 5]
+        for radiant, dire in zip(radiant_players, dire_players):
             player_rows += f"""
                 <div class="d-flex justify-content-between align-items-center mb-2">
                     <!-- Radiant -->
                     <div class="d-flex align-items-center">
-                        <img src="{escape(radiant["static_hero_icon_path"])}" alt="hero icon">
+                        <img 
+                            class="hero-icon"
+                            src="{escape(radiant["static_hero_icon_path"])}" 
+                            alt="hero icon {escape(radiant["hero_id"])}"
+                        >
                         <span class="ms-2">{escape(radiant["account_id"])}</span>
                     </div>
                     <!-- Dire -->
                     <div class="d-flex align-items-center flex-row-reverse">
-                        <img src="{escape(dire["static_hero_icon_path"])}" alt="hero icon">
+                        <img
+                            class="hero-icon"
+                            src="{escape(dire["static_hero_icon_path"])}"
+                            alt="hero icon {dire["hero_id"]}"
+                        >
                         <span class="me-2">{escape(dire["account_id"])}</span>
                     </div>
                 </div>
@@ -97,11 +116,19 @@ class LiveDotaGame:
                     <div class="col-12 mb-2">
                         {player_rows}
                     </div>
-                    <pre>{escape(self.players)}</pre>
+                    <br>
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="btn-group">
-                            <button type="button" class="btn btn-sm btn-outline-secondary">Copy Match ID</button>
+                            <button
+                                id="copy-match-id"
+                                type="button" 
+                                class="btn btn-sm btn-outline-secondary"
+                                data="{self.match_id}"
+                            >
+                                Copy Match ID
+                            </button>
                         </div>
+                        {self.get_gold_display()}
                         <small class="text-muted">{time}</small>
                     </div>
                 </div>
@@ -110,38 +137,58 @@ class LiveDotaGame:
         """
 
     def get_players_summary(self, players_response):
-        dota = opendota.OpenDota()
-        player_summary = [{} for _ in range(10)]
+        if not players_response:
+            print(f"WARNING: No players in match {self.match_id}")
+            return {"radiant": [], "dire": []}
+
+        radiant_players = []
+        dire_players = []
 
         for player in players_response:
-            try:
-                slot = player.get("team_slot")
-                if slot not in range(0, 10):
-                    continue
+            summary = {
+                "account_id": player.get("account_id", None),
+                "account_metadata": {}, # todo: player names
+                "hero_id": player.get("hero_id", None)
+            }
 
-                summary = {
-                    "account_id":       player.get("account_id", None),
-                    "account_metadata": {},
-                    "hero_id":          player.get("hero_id", None)
-                }
+            hero_id = summary["hero_id"]
+            if hero_id == None:
+                summary["static_hero_icon_path"] = f"static/images/dota2/hero/icon/unknown.png"
+            else:
+                summary["static_hero_icon_path"] = f"static/images/dota2/hero/icon/{hero_id}.png"
 
-                if summary["hero_id"] is not None:
-                    summary["static_hero_icon_path"] = f"static/images/dota2/hero/icon/{summary["hero_id"]}.png"
-                else:
-                    summary["static_hero_icon_path"] = "static/images/dota2/hero/icon/unknown/png"
+            team = player.get("team")
+            slot = player.get("team_slot")
 
-                player_summary[slot] = summary
-            except Exception:
-                continue
+            if team == 0:
+                radiant_players.append((slot, summary))
+            elif team == 1:
+                dire_players.append((slot, summary))
+            
+        radiant_sorted = [s for _, s in sorted(radiant_players, key=lambda x: x[0])]
+        dire_sorted = [s for _, s in sorted(dire_players, key=lambda x: x[0])]
 
-        # defaults
-        for i in range(10):
-            if not player_summary[i]:
-                player_summary[i] = {
-                    "account_id":            "???",
-                    "account_metadata":      {},
-                    "hero_id":               "unknown",
-                    "static_hero_icon_path": "static/images/dota2/hero/icon/unknown.png"
-                }
+        return {
+            "radiant": radiant_sorted,
+            "dire": dire_sorted
+        }
 
-        return player_summary
+    def get_gold_display(self):
+        if self.gold_lead >= 0:
+            return f"""
+                <span class='text-success'>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
+                        <path d="m280-400 200-200 200 200H280Z"/>
+                    </svg>
+                    {self.gold_lead}
+                </span>
+                """
+        else:
+            return f"""
+                <span class='text-danger'>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
+                        <path d="M480-360 280-560h400L480-360Z"/>
+                    </svg>
+                    {abs(self.gold_lead)}
+                </span>
+                """
--- src/sentry/static/js/home.js
+++ src/sentry/static/js/home.js
@@ -1,7 +1,24 @@
 $(document).ready(async function() {
     live = await getLiveGames();
-    // console.log(live);
-    $("#live-game-container").html(live)
+    $("#live-game-container").html(live);
+
+    // replace any missing hero icons (update manually)
+    $("#live-game-container").find("img.hero-icon").on("error", function() {
+        this.onerror = null;
+        this.src = 'data:image/svg+xml;utf8,' +
+            encodeURIComponent(`
+                <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#e3e3e3">
+                    <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm40-337 160-160 160 160 160-160 40 40v-183H200v263l40 40Zm-40 257h560v-264l-40-40-160 160-160-160-160 160-40-40v184Zm0 0v-264 80-376 560Z"/>
+                </svg>
+            `);
+    });
+
+    $(document).on("click", "#copy-match-id", function() {
+        const match_id = $(this).attr("data");
+        if (match_id) {
+            navigator.clipboard.writeText(match_id)
+        }
+    });
 });
 
 async function getLiveGames() {
